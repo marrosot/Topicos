581cc62904677e289f5245c92b4f4a9ecb9ee6d1
laboratorio <laboratorio@SP-LAB9-06>
2018-09-22 15:43:00 -0600

    Mi primera version de la aplicacion

---
+++ miAplicacion/miAplicacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="miAplicacion.MvcApplication" Language="C#" %>
---
+++ miAplicacion/miAplicacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(miAplicacion.Startup))]
+namespace miAplicacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
